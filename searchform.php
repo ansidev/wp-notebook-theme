6566d88c0a53242c27406f903fe1bf9c00b7026a
ansidev <ansidev@gmail.com>
2016-02-06 17:06:13 +0700

    Add search form.
    Signed-off-by: ansidev <ansidev@gmail.com>

---
+++ searchform.php
@@ -0,0 +1,3 @@
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+    <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'notebook'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s">
+</form>
